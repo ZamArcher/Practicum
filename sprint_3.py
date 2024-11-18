0d7f10bede22a6a65f13062e9d03f49f69273625
Aleksandr Zakharov <ZamArcher@gmail.com>
2024-11-18 22:19:14 +0500

    theme 3/7 lesson 4/6 complete

--- sprint_3.py
+++ sprint_3.py
@@ -38,6 +38,53 @@
 # print(test_string.isValid("([)]"))
 # print(test_string.isValid("(([])"))
 # ---------------------------
+# class Employee:
+#     vacation_days = 28
+
+#     def __init__(self, first_name, second_name, gender):
+#         self.first_name = first_name
+#         self.second_name = second_name
+#         self.gender = gender
+#         self.remaining_vacation_days = Employee.vacation_days
+
+#     def consume_vacation(self, days):
+#         self.remaining_vacation_days -= days
+
+#     def get_vacation_details(self):
+#         return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
+
+# # Расширьте класс Employee, создав классы FullTimeEmployee и PartTimeEmployee.
+# class FullTimeEmployee(Employee):
+
+#     def __init__(self, first_name, second_name, gender):
+#         super().__init__(first_name, second_name, gender)
+#         self.remaining_vacation_days = FullTimeEmployee.vacation_days
+
+#     def get_unpaid_vacation(self, start_date: str, days: int) -> str:
+#         return (
+#             f'Начало неоплачиваемого отпуска: {start_date}, '
+#             f'продолжительность: {days} дней.'
+#         )
+
+
+# class PartTimeEmployee(Employee):
+#     vacation_days = 14
+
+#     def __init__(self, first_name, second_name, gender):
+#         super().__init__(first_name, second_name, gender)
+#         self.remaining_vacation_days = PartTimeEmployee.vacation_days
+
+#     # def __init__(self, dial_type_value, network_type):
+#     #     super().__init__(remaining_vacation_days)
+
+
+# # Пример использования:
+# full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+# print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+# part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+# print(part_time_employee.get_vacation_details())
+# ----------------------------
 class Employee:
     vacation_days = 28
 
@@ -46,6 +93,10 @@ class Employee:
         self.second_name = second_name
         self.gender = gender
         self.remaining_vacation_days = Employee.vacation_days
+        self._employee_id = self.__generate_employee_id()
+
+    def __generate_employee_id(self):
+        return hash(self.first_name + self.second_name + self.gender)
 
     def consume_vacation(self, days):
         self.remaining_vacation_days -= days
@@ -54,14 +105,16 @@ class Employee:
         return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
 
 
-# Расширьте класс Employee, создав классы FullTimeEmployee и PartTimeEmployee.
 class FullTimeEmployee(Employee):
 
-    def __init__(self, first_name, second_name, gender):
+    def __init__(self, first_name, second_name, gender, __salary):
         super().__init__(first_name, second_name, gender)
-        self.remaining_vacation_days = FullTimeEmployee.vacation_days
+        self.__salary = __salary
 
-    def get_unpaid_vacation(self, start_date: str, days: int) -> str:
+    def __get_vacation_salary(self):
+        return 0.8 * self.__salary
+
+    def get_unpaid_vacation(self, start_date, days):
         return (
             f'Начало неоплачиваемого отпуска: {start_date}, '
             f'продолжительность: {days} дней.'
@@ -75,13 +128,13 @@ class PartTimeEmployee(Employee):
         super().__init__(first_name, second_name, gender)
         self.remaining_vacation_days = PartTimeEmployee.vacation_days
 
-    # def __init__(self, dial_type_value, network_type):
-    #     super().__init__(remaining_vacation_days)
-
 
 # Пример использования:
-full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+full_time_employee = FullTimeEmployee('Иван', 'Иванов', 'м', 50000)
 print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
-part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+
+part_time_employee = PartTimeEmployee('Анна', 'Петрова', 'ж')
+part_time_employee.consume_vacation(5)
 print(part_time_employee.get_vacation_details())
-# ----------------------------
+
+# print(full_time_employee._Employee__get_vacation_salary())
